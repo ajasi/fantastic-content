ae617168170c62175a56f5b823bea7fffec98347
ajasi <aleksandar.jasikovac@badin.rs>
2023-12-19 22:53:53 +0100

    Integrate LATEST_ARTICLES data list

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import ArticleItem from "./ArticleItem.vue";
+
+const state = ref({
+  articles: [],
+});
+
+onMounted(() => {
+  if (window.LATEST_ARTICLES) {
+    state.value.articles = window.LATEST_ARTICLES;
+  }
+});
 </script>
 
 <template>
