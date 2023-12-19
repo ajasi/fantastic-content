3aac77616895150d5f88db67110659243f7e1116
ajasi <aleksandar.jasikovac@badin.rs>
2023-12-19 23:17:45 +0100

    implement filtering functionality

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import ArticleItem from "./ArticleItem.vue";
 
 const state = ref({
+  categories: ["news", "essay"],
   articles: [],
 });
 
@@ -11,11 +12,37 @@ onMounted(() => {
     state.value.articles = window.LATEST_ARTICLES;
   }
 });
+
+const filteredArticles = computed(() => {
+  let lastSevenDays = new Date("2023-06-05"); // here should be today's date // new Date() but it wont be visible
+  lastSevenDays.setDate(lastSevenDays.getDate() - 7);
+
+  return state.value.articles
+    .filter((article) => state.value.categories.includes(article.category))
+    .filter((article) => new Date(article.publishDate) > lastSevenDays)
+    .sort((a, b) => new Date(b.publishDate) - new Date(a.publishDate))
+    .slice(0, 5);
+});
 </script>
 
 <template>
-  <div v-for="article in state.articles" :key="article.url">
-    <ArticleItem :article="article" />
+  <div class="container">
+    <h2>Latest Articles</h2>
+    <div class="article">
+      <div class="filters">
+        <label
+          ><input type="checkbox" v-model="state.categories" value="news" />
+          News</label
+        >
+        <label
+          ><input type="checkbox" v-model="state.categories" value="essay" />
+          Essay</label
+        >
+      </div>
+      <div v-for="article in filteredArticles" :key="article.publishDate">
+        <ArticleItem :article="article" />
+      </div>
+    </div>
   </div>
 </template>
 
